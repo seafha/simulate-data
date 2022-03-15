c6992e073fe448eb190fe28714906f4ce5da1406
Seafha Ramos <seafha@gmail.com>
2022-03-15 10:34:22 -0700

    Add code for simulating and plotting data

--- Simulate_plot_data.R
+++ Simulate_plot_data.R
@@ -4,7 +4,10 @@
 # 2022-03-15
 
 # Simulate predictor variable 
+x <- rnorm(n=100)
 
 # Calculate response variable and add noise
+y <- 2 * x + rnorm(n=100, sd = 0.2)
 
 # Plot the data
+plot(x = x, y = y)
